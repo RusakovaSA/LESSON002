4388b00593f1aeacb7faf10417e46ac193d769eb
rusakova slavyana <blondi_sl@mail.ru>
2023-04-09 13:23:17 +0300

    Классический-перебор-значений

---
+++ Example001/Program.cs
@@ -0,0 +1,26 @@
+﻿// Найти максимальное значение из 9
+
+int a = 4;
+int b = 7;
+int c = 1;
+int d = 3;
+int e = 4;
+int f = 15;
+int g = 2;
+int h = 30;
+int i = 8;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+if (f > max) max = f;
+if (g > max) max = g;
+if (h > max) max = h;
+if (i > max) max = i;
+
+Console.Write ("Максимальное значение из списка ");
+Console.WriteLine (max);
