491828fbe4cb28dd0affaf60ea730729e8799aef
rusakova slavyana <blondi_sl@mail.ru>
2023-04-12 22:00:22 +0300

    Поиск-find-в-массиве

---
+++ Example003/Program.cs
@@ -0,0 +1,19 @@
+﻿// Ищем find в массиве array
+
+int[] array = { 1, 2, 4, 6, 77, 8, 9, 10, 77 };
+
+int N = array.Length;
+int find = 77;
+int index = 0;
+
+while (index < N)
+{
+    if (array[index] == find)
+    {
+        Console.Write("Искомая позиция ");
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
