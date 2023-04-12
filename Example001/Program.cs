80ee7b2bfc256dd8778518e58c6e5a575012ba30
rusakova slavyana <blondi_sl@mail.ru>
2023-04-12 20:29:40 +0300

    Максимальное-значение-из-9

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,26 +1,32 @@
-﻿// Найти максимальное значение из 9
+﻿// Найти максимальное значение из 9, путем перебора тройками значений.
+
+int max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
 
 int a = 4;
 int b = 7;
 int c = 1;
-int d = 3;
+int d = 366666;
 int e = 4;
 int f = 15;
 int g = 2;
 int h = 30;
 int i = 8;
 
-int max = a;
+// int max1 = max (a, b, c);
+// int max2 = max (d, e, f);
+// int max3 = max (g, h, i);
+// int result = max (max1, max2, max3);
 
-if (a > max) max = a;
-if (b > max) max = b;
-if (c > max) max = c;
-if (d > max) max = d;
-if (e > max) max = e;
-if (f > max) max = f;
-if (g > max) max = g;
-if (h > max) max = h;
-if (i > max) max = i;
+int result = max(
+    max(a, b, c),
+    max(d, e, f),
+    max(g, h, i));
 
-Console.Write ("Максимальное значение из списка ");
-Console.WriteLine (max);
+Console.Write("Максимальное значение из списка ");
+Console.WriteLine(result);
