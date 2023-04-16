68c71418174a0dcfb6489159b7411eb46d8c5657
rusakova slavyana <blondi_sl@mail.ru>
2023-04-16 19:06:28 +0300

    кратно-некратно-7-23

---
+++ Example008/Program.cs
@@ -0,0 +1,36 @@
+﻿// 14. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+Console.WriteLine("Введите число 1: ");
+int a = int.Parse(Console.ReadLine());
+
+bool c = a % 7 ==0 && a % 23 ==0;
+
+if (c)
+{
+    Console.WriteLine("Число " + a + " кратно 7 и 23 "  );
+}
+else
+{
+    Console.WriteLine("Число " + a + " не кратно 7 и 23");
+}
+
+
+
+// int a = int.Parse(Console.ReadLine());
+
+// bool OutputKratnoLi(int a)
+// {
+//     if ((a % 7 == 0) && (a % 23 == 0))
+//     {
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+
+// }
+// Console.WriteLine(OutputKratnoLi(a));
