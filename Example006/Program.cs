d580ffa31abab5c170899537407982dcc53c191a
rusakova slavyana <blondi_sl@mail.ru>
2023-04-16 18:38:31 +0300

    первая-последняя-цифра-трехзначного-числа

---
+++ Example006/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.Write("Случайное трехзначное число ");
+Console.WriteLine(number);
+int num1 = number / 100;
+int num3 = number % 10;
+Console.Write("Первая и последняя цифры числа образуют число ");
+Console.WriteLine($"{num1}{num3}");
