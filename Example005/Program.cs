abd0ca7e0d48226940806890f26241fbe6993328
rusakova slavyana <blondi_sl@mail.ru>
2023-04-16 17:03:21 +0300

    Вывод-случайного-числа-наибольшая-наименьшая-цифра

--- Example005/Program.cs
+++ Example005/Program.cs
@@ -1,4 +1,41 @@
 ﻿// Вывести случайное число от 10 до 99
 // Показать наибольшую цифру числа
 
+Console.Write("Вывести случайное число от ");
+int min = ReadInt();
+Console.Write(" до ");
+int max = ReadInt();
 
+int number = GetRandomNumber(min, max);
+
+Console.Write("Случайное число ");
+Console.WriteLine(number);
+
+if (GetFirstDigit(number) > GetSecondDigit(number))
+{
+    Console.WriteLine("Первая цифры больше второй");
+}
+else
+{
+    Console.WriteLine("Второая цифра больше первой");
+}
+
+int GetRandomNumber(int min, int max)
+{
+    return new Random().Next(min, max + 1);
+}
+
+int GetFirstDigit(int number)
+{
+    return number / 10;
+}
+
+int GetSecondDigit(int number)
+{
+    return number % 10;
+}
+
+int ReadInt()
+{
+    return int.Parse(Console.ReadLine());
+}
